8ffac44e14dc7d00e8356d0c914e2dc7c483c3e8
APXEOLOG <apxeolog.ru@gmail.com>
2020-03-06 12:27:59 +0300

    fix: Update script and add missing icon

--- create-distribution.sh
+++ create-distribution.sh
@@ -71,9 +71,9 @@ http {
     location /api/ {
       proxy_pass http://127.0.0.1:5000;
       proxy_http_version 1.1;
-      proxy_set_header Host $host;
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Host \$host;
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     }
   }
 }
